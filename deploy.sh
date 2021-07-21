1c0f80ee44efe03c562766a4bfaff9a412d74319
v_djqding <v_djqding@tencent.com>
2021-07-21 17:52:48 +0800

    修改base地址

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ yarn docs:build
 
 rm -rf build.tar
 tar -zcvf build.tar .docs/.vuepress/dist
-sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no root@47.107.225.230
+sshpass -p $PASSWORD scp -o StrictHostKeyChecking=no ./build.tar root@47.107.225.230:/root/
