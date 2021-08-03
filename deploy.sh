b57ed9ab53faf94070856e5b3a18c2ab404c246b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-08-03 21:24:51 +0800

    Deploy DingJiaQin/DingJiaQin.github.io to github.com/DingJiaQin/DingJiaQin.github.io.git:gh-pages

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+# 生成静态文件
+yarn docs:build
+
+rm -rf build.tar
+tar -zcvf build.tar ./docs/.vuepress/dist
+sshpass -p $PASSWORD scp -o StrictHostKeyChecking=no ./build.tar root@47.107.225.230:/var/www/
+sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no root@47.107.225.230 "cd /var/www/ && rm -rf docs html && tar -zxvf build.tar  && mv ./docs/.vuepress/dist ./html"
---
+++ docs/myblog/README.md
@@ -0,0 +1,17 @@
+# 自我介绍
+::: tip 基本信息哈哈哈哈成功啦
+这是基本信息
+:::
+
+``` js
+// 注释
+```
+:tada: :100: :bamboo: :gift_heart: :fire: :relieved: :smile:
+
+# 我的头像
+![图片](http://47.107.225.230/public/myLogo.jpg)
+<!-- <script>
+  export default{
+    console.log('可以在这里打印啦～～～～');
+}
+</script> -->
