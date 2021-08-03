d7d538ccc3218b99c4f7673f664a332fed59bac9
DingJiaQin <18309895899@163.com>
2021-08-03 16:10:05 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ yarn docs:build
 rm -rf build.tar
 tar -zcvf build.tar ./docs/.vuepress/dist
 sshpass -p $PASSWORD scp -o StrictHostKeyChecking=no ./build.tar root@47.107.225.230:/var/www/
-sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no root@47.107.225.230 "cd /var/www/ && rm -rf docs html && tar -zxvf build.tar  && mv ./docs/.vuepress/dist ./html"
+sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no root@47.107.225.230 "cd /var/www/ && rm -rf docs html && tar -zxvf build.tar  && mv ./docs/.vuepress/dist/myblog ./html"
